532183e7cd8cd3888648c5902d14d557edd7ba9a
League Student <league@iMac-14.attlocal.net>
2019-08-13 14:25:55 -0700

    day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,29 +6,41 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	
+	Robot c3po = new Robot ();
 
 	void go() {
+		c3po.penDown();
 		// 4. Make the robot move as fast as possible
+		c3po.setSpeed(1000000000);
 
 		// 5. Set the pen width to 5
-
+c3po.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for(int i = 0; i<4; i++) {
 			// 7. Set the pen color to random
-	
+	c3po.setRandomPenColor();
 			// 1. Call the drawSquare() method
+		drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
-	}
-
+c3po.turn(90);
+}}
 	
-	void drawSquare() {
+	
+	void drawSquare() {{
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
 		
+		for(int i = 0; i<4; i++) {
+			c3po.move(200);
+			c3po.turn(90);
+			}
+			
+		}
 		
 	}
+
 
 	public static void main(String[] args) {
 		new FourSquare().go();
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+				Robot c3po = new Robot ();
+		c3po.penDown();
+		c3po.setSpeed(200);
+	for(int i = 0; i<4; i++) {
+	c3po.move(200);
+	c3po.turn(90);
+	}
 	
-		
-		
 	}
 }
